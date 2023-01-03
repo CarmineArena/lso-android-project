c724d203e4a4499f62559894258f20c69e8996a4
Simone Catapano <catapano.smn.2001@gmail.com>
2023-01-03 16:29:23 +0100

    The send_flag() function does not work

--- app/src/main/java/com/example/history4fun/Client.java
+++ app/src/main/java/com/example/history4fun/Client.java
@@ -7,6 +7,8 @@ public class Client {
     private final int server_port = 6969;
     private Socket client_socket = null;
     private InetAddress server_address = null;
+    PrintWriter out_toServer = null;
+    BufferedReader in_fromServer = null;
 
     public Client() {
         connect();
@@ -16,6 +18,17 @@ public class Client {
         try {
             server_address = InetAddress.getByName("10.0.2.2");
             client_socket = new Socket(server_address, server_port);
+
+            out_toServer = new PrintWriter(client_socket.getOutputStream(), true);
+            in_fromServer = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
+
+            String message_to_send = "Hello! I am the client!";
+            out_toServer.println(message_to_send);
+            out_toServer.flush();
+            String message_received = in_fromServer.readLine();
+
+            out_toServer.close();
+            in_fromServer.close();
         } catch(UnknownHostException e) {
             e.printStackTrace();
         } catch(IOException e) {
@@ -23,19 +36,20 @@ public class Client {
         }
     }
 
-    public void handle_connection() {
+    public void send_flag(String flag) {
         try {
-            PrintWriter out_toServer = new PrintWriter(client_socket.getOutputStream(), true);
-            BufferedReader in_fromServer = new BufferedReader(new InputStreamReader(client_socket.getInputStream()));
-
-            String message_to_send = "Hello! I am the client!";
-            out_toServer.println(message_to_send);
+            out_toServer = new PrintWriter(client_socket.getOutputStream(), true);
+            out_toServer.println(flag);
             out_toServer.flush();
-
-            String message_received = in_fromServer.readLine();
-
             out_toServer.close();
-            in_fromServer.close();
+            close_connection();
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void close_connection() {
+        try {
             client_socket.close();
         } catch(IOException e) {
             e.printStackTrace();
--- app/src/main/java/com/example/history4fun/MainActivity.java
+++ app/src/main/java/com/example/history4fun/MainActivity.java
@@ -1,21 +1,41 @@
 package com.example.history4fun;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import java.lang.Thread;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
     private Client client = null;
+    private Button login_button = null;
+    private EditText mail_text = null;
+    private EditText pass_text = null;
+
+    void handle_login_button(Client client) {
+        login_button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String email = String.valueOf(mail_text.getText());
+                String password = String.valueOf(pass_text.getText());
+
+                client.send_flag("LOGIN");
+                // handle errors!
+            }
+        });
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        login_button = (Button) findViewById(R.id.button);
+        mail_text = (EditText) findViewById(R.id.MailText);
+        pass_text = (EditText) findViewById(R.id.editTextTextPassword);
+
         Thread client_thread = new Thread(() -> {
             client = new Client();
-            client.connect();
-            client.handle_connection();
+            handle_login_button(client);
         });
         client_thread.start();
     }
